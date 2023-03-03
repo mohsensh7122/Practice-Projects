ea5a6ac157fb3b266544b05a66179e2ff425d712
Mohsen Shirazi <mohsensh4792@gmail.com>
2023-03-03 10:26:52 -0500

    Completed the "selecting-DOM-elements" practice.

--- selecting-DOM-elements/js/scripts.js
+++ selecting-DOM-elements/js/scripts.js
@@ -1,25 +1,44 @@
 // 1: Select the element with the ID 'about'.
 //    Store the element in the variable `about`.
-const about;
+const about = document.getElementById('about');
 about.style.border = "2px solid firebrick";
 
 // 2: Select all the <h2> elements in the document.
 //    Set the color of the <h2> elements to a different color.
 
+const secondaryHeading = document.querySelectorAll('h2');
+
+for (let i = 0; i < secondaryHeading.length; i++){
+    secondaryHeading[i].style.color = 'red';
+}
 
 // 3: Select all elements with the class '.card'.
 //    Set their background color to the color of your choice.
 
+const cards = document.querySelectorAll('.card');
+
+for (let i = 0; i < cards.length; i++){
+    cards[i].style.backgroundColor = 'skyblue';
+}
 
 // 4: Select only the first <ul> in the document.
 //    Assign it to a variable named `ul`.
+
+const ul = document.querySelector('ul');
 
 ul.style.border = "2px solid indigo";
 
 // 5: Select only the second element with the class '.container'.
 //    Assign it to a variable named `container`.
 
+const container = document.querySelector('.container:nth-of-type(2)');
 container.style.backgroundColor = "royalblue";
 
 // 6: Select all <a> elements that have a 'title' attribute.
 //    Set their color value to the color of your choice.
+
+const titles = document.querySelectorAll('a[title]');
+
+for (let i = 0; i < titles.length; i++){
+    titles[i].style.color = 'green';
+}
